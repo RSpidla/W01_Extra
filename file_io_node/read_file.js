55d5d7608d273fa9e4b78adec3cfdd45193943c1
‘RSpidla’ <rspidla@gmail.com>
2019-02-09 00:29:29 +0000

    Final version

--- file_io_node/read_file.js
+++ file_io_node/read_file.js
@@ -1,22 +1,22 @@
 // Synchronous file write
-// let fs = require("fs");
+let fs = require("fs");
 
-// let filePath = "/tmp/test_sync.txt";
-// let fileData = "Testing synchronous file write. \n";
+let filePath = "./tmp/test_sync.txt";
+let fileData = "Testing synchronous file write. \n";
 
-// fs.writeFileSync(filePath, fileData);
-// console.log("successfully wrote to", filePath);
+fs.writeFileSync(filePath, fileData);
+console.log("successfully wrote to", filePath);
 
 
 // Asynchronous file write
-let fs = require("fs");
+// let fs = require("fs");
 
-let filePath = "/tmp/test_async.txt";
-let fileData = "Testing asynchronous file write. \n";
+// let filePath = "/tmp/test_async.txt";
+// let fileData = "Testing asynchronous file write. \n";
 
-fs.writeFile(filePath, fileData, function(err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Successfully wrote to", filePath);
-});
+// fs.writeFile(filePath, fileData, function(err) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Successfully wrote to", filePath);
+// });
